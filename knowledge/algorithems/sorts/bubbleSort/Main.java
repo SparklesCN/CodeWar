f6997e8103817c72668a291d55c6a5917fcfc164
Sparkles <nathanquan.us@gmail.com>
2019-10-22 12:37:05 -0700

    auto published in gh-pages branch

--- knowledge/algorithems/sorts/bubbleSort/Main.java
+++ knowledge/algorithems/sorts/bubbleSort/Main.java
@@ -3,28 +3,26 @@ import java.io.*;
 
 public class Main {
 
-    public static int[] bubbleSort(int[] intArray) {
-        for (int end = intArray.length - 1; end > 0; end--) {
-            for (int i = 0; i < end; i++) {
-                if (intArray[i] > intArray[i+1]) {
-                    intArray = swap (i, i+1, intArray);
+    public static void bubbleSort(int[] inputArray) {
+        for (int end = inputArray.length - 1; end > 0; end--) {
+            for (int pointer = 0; pointer < end; pointer++) {
+                if (inputArray[pointer] > inputArray[pointer+1]) {
+                    swap(inputArray, pointer, pointer+1);
                 }
             }
         }
-        return intArray;
     }
 
-    public static int[] swap (int a, int b, int[] intArray) {
-		int temp = intArray[a];
-		intArray[a] = intArray[b];
-		intArray[b] = temp;
-		return intArray;
+    public static void swap(int[] inputArray, int indexA, int indexB) {
+        int temp = inputArray[indexA];
+        inputArray[indexA] = inputArray[indexB];
+        inputArray[indexB] = temp;
     }
-    
+
     public static void main(String[] args) {
-		int[] array = {5, 3, 0, 2 ,1 ,4, -1};
-		System.out.println(Arrays.toString(array));
-		array = bubbleSort(array);
+        int[] array = {5, 3, 0, 2 ,1 ,4, -1};
         System.out.println(Arrays.toString(array));
-	}
+        bubbleSort(array);
+        System.out.println(Arrays.toString(array));
+    }
 }
